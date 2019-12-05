6c75ca0e2a776cd36072b0235c777ad521e719d6
sagy <sagy@smart-farm.co>
2019-12-05 13:46:22 +0200

    removed spaces and final

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -13,7 +13,6 @@ class TodosController extends Controller
     public function index()
     {
         $todos = Todo::all();
-
         return view('todos')->with('todos', $todos);
     }
 
@@ -37,9 +36,7 @@ class TodosController extends Controller
     public function delete($id)
     {
         $todo = Todo::find($id);
-
         $todo->delete();
-
         session()->flash('success', 'Your todo was Deleted!');
         return redirect()->back();
     }
@@ -47,20 +44,15 @@ class TodosController extends Controller
     public function update($id)
     {
         $todo = Todo::find($id);
-
         return view('update')->with('todo', $todo);
     }
 
     public function save(Request $request, $id)
     {
         //dd($request->all()); //To check
-
         $todo = Todo::find($id);
-
         $todo->todo = $request->todo; //saving what i'm getting form the update
-
         $todo->save();
-
         session()->flash('success', 'Your todo was Saved!');
         return redirect()->route('todos');
     }
@@ -68,11 +60,8 @@ class TodosController extends Controller
     public function completed($id)
     {
         $todo = Todo::find($id);
-
         $todo->completed = 1;
-
         $todo->save();
-
         session()->flash('success', 'Your todo was Marked as Completed!');
         return redirect()->back();
     }
